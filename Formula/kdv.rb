0c4624ed5d91dd56ca1346380a2a2a6d71b6b34f
Wes Widner <kai5263499@gmail.com>
2016-07-31 08:30:52 -0400

    kdv 0.2 (new formula)

---
+++ Formula/kdv.rb
@@ -0,0 +1,18 @@
+class Kdv < Formula
+
+  desc "CLI to access OSX/iOS's KDebug facility"
+  homepage "http://www.newosxbook.com/tools/kdv.html"
+  url "http://www.newosxbook.com/tools/kdv.tar"
+  version "0.2"
+  sha256 "e9357ad121cb6decf121a900f3a46e05e1a3717446fbbc7663de2a799de22fe6"
+
+  def install
+    mv "kdv.universal", "kdv"
+    bin.install "kdv"
+
+  end
+
+  test do
+    assert_match "This is J's kdebugview version 0.2", shell_output("#{bin}/kdv 2>&1")
+  end
+end

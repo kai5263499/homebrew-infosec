d9294c13fbe313a46e5882eee62c352c7bab4772
Wes Widner <kai5263499@gmail.com>
2016-08-01 16:25:37 -0400

    binspect 1.0 (new formula)

---
+++ Casks/binspect.rb
@@ -0,0 +1,8 @@
+cask 'binspect' do
+  version '1.0'
+  sha256 '1bfac5a614b50090e4ca30fe583661df9408d1909cb49d0a37745e29d3e75d54'
+
+  url 'http://mac-infosec-tools.s3-website-us-east-1.amazonaws.com/binspect.dmg'
+  name 'binspect'
+  homepage 'https://github.com/joesavage/binspect'
+end

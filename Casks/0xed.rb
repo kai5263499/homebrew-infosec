f05a04f757b280a94445285a393c61a7d1331384
Wes Widner <kai5263499@gmail.com>
2016-08-20 09:47:22 -0400

    0xED 1.1.3 (new formula)

---
+++ Casks/0xed.rb
@@ -0,0 +1,8 @@
+cask '0xed' do
+  version '1.1.3'
+  sha256 '56b68898c7a8c1169e9f42790091b100841c3d43549445dc3c4986fae7152793'
+
+  url 'http://www.suavetech.com/cgi-bin/download.cgi?0xED.tar.bz2'
+  name '0xed'
+  homepage 'http://www.suavetech.com/0xed/'
+end

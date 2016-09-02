8c1c42b08de63dfd0ab25cbce621ae2cbec697e2
Wes Widner <kai5263499@gmail.com>
2016-09-01 22:00:54 -0400

    add resknife (new formula)

---
+++ Casks/resknife.rb
@@ -0,0 +1,8 @@
+cask 'resknife' do
+  version '0.6'
+  sha256 '637bd1b715043bacf477076a2d20ef558bf8fb3b3000d6e1f1d7aa1ce93f1094'
+
+  url 'https://github.com/downloads/slobo/ResKnife/ResKnife%20Cocoa.zip'
+  name 'reskinfe'
+  homepage 'http://resknife.sourceforge.net/'
+end

9b2135de789ace38691cb586631f3e669bdc7e62
Wes Widner <kai5263499@gmail.com>
2016-09-05 09:59:16 -0400

    copying two utilities, lingon-x and knockknock that are well-suited for osx incident response

---
+++ Casks/knockknock.rb
@@ -0,0 +1,12 @@
+cask 'knockknock' do
+  version '1.9.0'
+  sha256 '3af57a0689d9a6c28b1cb3a0c9624b832b1d77a17d9af5c2d3df9b05c709072e'
+
+  # bitbucket.org/objective-see was verified as official when first introduced to the cask
+  url "https://bitbucket.org/objective-see/deploy/downloads/KnockKnock_#{version}.zip"
+  name 'KnockKnock'
+  homepage 'https://objective-see.com/products/knockknock.html'
+  license :gratis
+
+  app 'KnockKnock.app'
+end
